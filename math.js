430b78ef50a208a5aae36881b38058dfb41f25d7
saka-bta <bta.msru@gmail.com>
2022-02-03 03:07:53 +0600

    math-swap

---
+++ math.js
@@ -0,0 +1,13 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+const num = 2.5;
+// const output = Math.ceil(num);
+// const output = Math.floor(num);
+// const output = Math.round(num);
+for (let i = 0; i <= 6; i++) {
+    const output = Math.random() * 6;
+    const rounded = Math.round(output);
+    console.log(rounded);
+}
---
+++ swap.js
@@ -0,0 +1,12 @@
+let first = 5;
+let second = 7;
+console.log(first, second);
+
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// desturcturing
+[first, second] = [second, first];
+console.log(first, second);
